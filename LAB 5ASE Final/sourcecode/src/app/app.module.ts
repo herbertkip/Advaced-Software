610684c13547aaf6bd25305c6667c77e7402adda
herbertkip <35582783+herbertkip@users.noreply.github.com>
2019-04-07 17:31:17 -0500

    Add files via upload

---
+++ LAB 5ASE Final/sourcecode/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { Facebook } from '@ionic-native/facebook';
+import {AngularFireModule} from "angularfire2";
+import {AngularFireAuthModule} from 'angularfire2/auth'
+import { GooglePlus } from '@ionic-native/google-plus';
+import { LinkedIn } from '@ionic-native/linkedin';
+import { TwitterConnect } from '@ionic-native/twitter-connect';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqDMI6TjxZ2BcKJChOdHRAUWXO3pj2OSk",
+    authDomain: "ionic-4-firebase-login-79360.firebaseapp.com",
+    databaseURL: "https://ionic-4-firebase-login-79360.firebaseio.com",
+    projectId: "ionic-4-firebase-login-79360",
+    storageBucket: "ionic-4-firebase-login-79360.appspot.com",
+    messagingSenderId: "419420722077"
+};
+export class AppModule {}
